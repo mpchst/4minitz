ffaf621acf4a3a9e9363cfcf70743bf036471b49
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2016-05-07 12:03:26 +0200

    added stub-collections package / used stub collections for meeting series test

--- test/unit/import/MeetingSeries.test.js
+++ test/unit/import/MeetingSeries.test.js
@@ -1,9 +1,16 @@
 import { expect } from 'meteor/practicalmeteor:chai';
 import { sinon } from 'meteor/practicalmeteor:sinon';
 
+import StubCollections from 'meteor/hwillson:stub-collections';
+import { MeetingSeriesCollection } from '/imports/collections/meetingseries_private'
+
 import { MeetingSeries } from '/imports/meetingseries'
 
-
+Meteor.methods({
+    'removeAll': () => {
+        MeetingSeriesCollection.remove({});
+    }
+});
 
 describe('MeetingSeries', function() {
 
@@ -14,6 +21,14 @@ describe('MeetingSeries', function() {
             project: "TestProject",
             name: "SeriesName"
         };
+
+        // mock our meetingSeriesCollection
+        StubCollections.stub(MeetingSeriesCollection);
+        Meteor.call('removeAll');
+    });
+
+    afterEach(function() {
+        StubCollections.restore();
     });
 
     it('Create new meeting series', function () {
@@ -23,6 +38,17 @@ describe('MeetingSeries', function() {
         testSeries.save();
 
         expect(spyOnMeteorCalls).calledWith('meetingseries.insert', testSeries);
+
+        // now we should found our new inserted series in the database
+        // and no series else
+        expect(MeetingSeriesCollection.find().count()).to.equal(1);
+        let seriesFromDb = MeetingSeriesCollection.findOne();
+        // the id should be the same
+        expect(seriesFromDb._id).to.equal(testSeries._id);
+        // project should be the same
+        expect(seriesFromDb.project).to.equal(testSeries.project);
+        // name should be the same
+        expect(seriesFromDb.name).to.equal(testSeries.name);
 
         spyOnMeteorCalls.restore();
     });
